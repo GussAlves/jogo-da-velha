411fac796ef5f19ad94403caeeaba9d512db0055
gusal <gus.alves.araujo@gmail.com>
2021-12-09 18:39:18 -0300

    Iniciando projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+import { Tabuleiro } from './domain/Tabuleiro.js'
+
+let tabuleiro = new Tabuleiro()
+
+tabuleiro.exibirTabuleiro()
+
+tabuleiro.alterarLinha('x', 1, 5)
+
+tabuleiro.exibirTabuleiro()
